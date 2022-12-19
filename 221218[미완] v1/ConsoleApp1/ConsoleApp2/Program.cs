19fca38f6b692916b4f1881c2baf0ae7ea1a148e
00woey <peace3064@naver.com>
2022-12-19 11:12:49 +0900

    up

---
+++ 221218[미완] v1/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Txttext rdtxt = new Txttext();
+            rdtxt.wordcnt();
+        }
+    }
+
+    class Txttext
+    {
+        string[] rdtxt;
+        string fulltxt;
+        int linecnt = 0; //라인수 체크용
+        int wdcnt = 0; //단어수 체크용
+        public Txttext()
+        {
+            rdtxt = File.ReadAllLines(@"course-1.txt"); //한줄씩 읽기
+            for(int i = 0; i < rdtxt.Length; i++)
+            {
+                linecnt++;
+                fulltxt += rdtxt[i];
+            }
+            Console.WriteLine("라인수: " + linecnt);
+        }
+
+        public void wordcnt()
+        {
+            for(int a = 0; a < fulltxt.Length; a++) //공백 개수로 단어수 예상
+            {
+                if (fulltxt[a] == ' ')
+                {
+                    wdcnt++;
+                }
+            }
+            Console.WriteLine("단어수: " + wdcnt);
+        }
+    }
+
+
+}
